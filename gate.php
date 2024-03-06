2df704a1fb7b597dd8a6702632b010168e2ee250
Binsar Harseno <binsar@expatify.co.id>
2024-03-06 14:58:07 +0700

    Update

--- gate.php
+++ gate.php
@@ -644,7 +644,11 @@ class Drew
     }
 
     $json_answers = json_encode($formattedAnswers, JSON_UNESCAPED_SLASHES);
-    echo $json_answers;
+    echo "ID CUSTOMER: ".$post['id_customer']."\n";
+    echo "RAW: ".$post['survey']."\n";
+    echo "DECODED: ".$decodedAnswers."\n";
+    echo "WITHOUT: ".$formattedAnswers."\n";
+    echo "JSON_UNESCAPED_SLASHES: ".$json_answers."\n";
     $curl = curl_init();
     curl_setopt_array($curl, array(
       CURLOPT_URL => 'https://drewcareid.myshopify.com/admin/api/2023-07/customers/'.$post['id_customer'].'.json',
