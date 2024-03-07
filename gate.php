014ef844ae9fab8921ce4f2f9c9a620ebb7eb24e
Binsar Harseno <binsar@expatify.co.id>
2024-03-07 09:23:51 +0700

    Update

--- gate.php
+++ gate.php
@@ -655,7 +655,7 @@ class Drew
       CURLOPT_TIMEOUT => 0,
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => 'PUT',
+      CURLOPT_CUSTOMREQUEST => 'POST',
       CURLOPT_POSTFIELDS =>'{
         "metafield": {
             "namespace": "custom",
