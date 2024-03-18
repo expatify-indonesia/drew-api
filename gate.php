04ea7735a6840bfbff1644bfa344ab6a1dca4465
Binsar Harseno <binsar@expatify.co.id>
2024-03-18 14:03:10 +0700

    Update

--- gate.php
+++ gate.php
@@ -9,9 +9,9 @@ class Drew
   public $campaign_webhook = 'https://api.customer.io/v1/webhook/80924986557a4646';
   public $graphQLUrl = 'https://drewcareid.myshopify.com/admin/api/2024-01/graphql.json';
   public $headers = array(
-      'X-Shopify-Access-Token: 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',
-      'Content-Type: application/json'
-    );
+    'X-Shopify-Access-Token: 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',
+    'Content-Type: application/json'
+  );
 
   public function __construct()
   {
